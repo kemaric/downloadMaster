9d6968757131c44b2a7f6544db1340c59ae4fca7
kemaric <kemaric@gmail.com>
2013-11-17 06:06:09 -0500

    added local storage

--- src/js/popup.js
+++ src/js/popup.js
@@ -13,67 +13,18 @@ $(document).ready(function(){
     }
     $('input[type="text"]').prop("disabled", true);
 
-
-/*    var inputEdit = document.createElement("input");
-    inputEdit.type = "button";
-    inputEdit.value="Edit" 
-    inputEdit.id="editUrl"
-    //$(forms).append(inputEdit); // put it into the DOM
-    $("#addUrl").replaceWith(inputEdit);*/
-
     var inputUrl = document.createElement("input");
     inputUrl.type = "text";
     inputUrl.placeholder = "url";
-    $(forms).append(inputUrl); // put it into the DOM
 
     var inputfolder = document.createElement("input");
-    inputfolder.type = "text";
-    inputfolder.placeholder = "folder";
-    $(forms).append(inputfolder); // put it into the DOM
-    $(forms).append($("#addUrl"));
-
-    save(x,y);
+	$("#edit").append("<li>" + x + " -> " + y + "</li>");
+	localStorage.setItem('list', edit.innerHTML);
+   // save(x,y);
+   $('input[type="text"]').prop("disabled", false);
+   $('input[type="text"]').val('');
 
   });
-
-  $("#editUrl").click(function(){
-    $('input[type="text"]').closest().prop("disabled", false);
-    var inputAdd = document.createElement("input");
-    inputAdd.type = "button";
-    inputAdd.value="Add" 
-    inputAdd.id="addUrl"
-    //$(forms).append(inputAdd); // put it into the DOM
-    $("#editUrl").replaceWith(inputAdd);
-  });
-
-
-  function save(x,y) {
-/*    chrome.storage.local.set({'urls': x});
-    chrome.storage.local.set({'folders': y});*/
-
-     var result = {'urls':x, 'folders':y};
-
-    // next we will push each individual results object into an array
-/*    chrome.storage.get('results',function(item){
-      item.results.push(result);
-      chrome.storage.set({'results':item.results});
-    });*/
-    // by passing an object you can define default values e.g.: []
-    chrome.storage.local.get({urls: []}, function (result) {
-        // the input argument is ALWAYS an object containing the queried keys
-        // so we select the key we need
-        var urls = result.urls;
-        urls.push({folders: x, HasBeenUploadedYet: false});
-        // set the new array value to the same key
-        chrome.storage.local.set({urls: y}, function () {
-            // you can use strings instead of objects
-            // if you don't  want to define default values
-            chrome.storage.local.get('urls', function (result) {
-                console.log(result.urls)
-            });
-        });
-    });
-  }
 
 
 /*  function getResults(newsId){
@@ -94,23 +45,15 @@ $(document).ready(function(){
 
 
   function load() {
-      var urls = "";
-      var folders = "";
-      chrome.storage.local.get('urls', function (result) {
-          urls = result.urls;
-          //alert(result.urls);
-          console.log(result.urls);
-          // $("#urls").val(urls);
-      });
+ 		// when the page loads
+	  if ( localStorage.getItem('list') ) {
+		edit.innerHTML = localStorage.getItem('list'); 
+	  }
+  }
 
-      chrome.storage.local.get('folders', function (result) {
-          folders = result.folders;
-          console.log(result.folders);
-          //alert(result.folders);
-          // $("#folders").val(folders);
-      });
-  } 
-
-
-
+$("#signInButton").click(function(){
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/client:plusone.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	  });  
 });
